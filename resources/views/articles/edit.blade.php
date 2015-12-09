3603188988f926d3c5d707cf4818de370d6a57bb
Andrii Pryimak <Andrii.Pryimak@live.com>
2015-12-10 01:49:53 +0200

    Add buttons to the edit.blade

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -4,17 +4,23 @@
     <meta charset="UTF-8">
     <title>Document</title>
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 </head>
 <body>
 
-@extends('app')
+{{--@extends('app')--}}
 
 @section('content')
 
     <h1>Edit: {!! $article->title !!}</h1>
 
+    <div id="imageHolder">
 
 
+
+    </div>
+    <button id="getimgButt">Search image</button>
+    <button id="saveimgButt">Insert image</button>
 
 
      {!! Form::model($article,['method'=>'PATCH', 'action'=>['ArticlesController@update', $article->id]]) !!}
@@ -25,7 +31,29 @@
 
     @include('errors.list')
 
-
+    <script>
+        $(document).ready(function(){
+            $("#getimgButt").click(function(){
+                $.ajax({
+                    method:"GET",
+                    data:{title: "{{$article->title}}"},
+                    url:"{{url('articles/getImage')}}",
+                }).done(function(data){
+                    console.log(data.img);
+                    $("#imageHolder").html("<img width='100' height='100' src=" + data.img + " />");
+                    $("#imageInput").val(data.img);
+                });});
+                $("#saveimgButt").click(function(){
+                    $.ajax({
+                        method:"GET",
+                        data:{articleId: "{{$article->id}}", imageUrl: $("#imageInput").val()},
+                        url:"{{url('articles/saveImage')}}",
+                    }).done(function(data){
+                        alert(data.answer);
+                    });
+            });
+        });
+    </script>
 @stop
 </body>
 </html>
