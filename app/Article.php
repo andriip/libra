fedd0e74c4da86df904257a55ebcec66ce2defe4
Andrii Pryimak <Andrii.Pryimak@live.com>
2015-12-09 23:21:30 +0200

    Create new model (Article.php)

---
+++ app/Article.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class Article extends Model
+{
+    //
+    protected $fillable=[
+        'title',
+        'body',
+        'published_at',
+        'image',
+    ];
+    protected $dates=['published_at'];
+
+    public function scopePublished($query)
+    {
+        $query->where('published_at', '<=', Carbon::now());
+    }
+
+
+    public function scopeUnPublished($query)
+    {
+        $query->where('published_at', '=>', Carbon::now());
+    }
+
+    public function setPublishedAtAttribute($date)
+    {
+        $this->attributes['published_at']=Carbon::parse( $date);
+    }
+
+    public function  user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
